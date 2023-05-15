99966207f1e103ac83656e26ace4985ee16c0343
Igor Malis <igordmalis@gmail.com>
2023-05-15 15:45:17 -0700

    Cleanup

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-import searchReducer from "./searchSlice";
 import indicatorReducer from "./indicatorSlice";
 import userInfoReducer from "./userInfoSlice";
 import playersTableReducer from "./playersTableSlice";
@@ -12,7 +11,6 @@ export const store = configureStore({
   reducer: {
     indicator: indicatorReducer,
     userInfo: userInfoReducer,
-    search: searchReducer,
     playersApi: playersApi.reducer,
     playersTable: playersTableReducer,
     playersData: playersDataReducer,
